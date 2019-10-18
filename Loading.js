f1e9bc7b3749b8e1ff0cf0e86b58fe64d0d4bf96
정연수 <jys1015@hanyang.co.kr>
2019-10-18 17:35:52 +0900

    1.1 LoadingScreen Complete

--- Loading.js
+++ Loading.js
@@ -3,12 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Loading(){
     return <View style={styles.container}>
-        <Text>Getting the current Weather</Text>
+        <Text style={styles.text}>Getting the current Weather</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex:1
+        flex:1,
+        justifyContent:"flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor:"#FDF6AA"
+    },
+    text:{
+        color:"#2c2c2c",
+        fontSize: 30
     }
 });
