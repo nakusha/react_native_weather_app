84b2a5267b68736d3f717025b8a8382057bc4486
정연수 <jys1015@hanyang.co.kr>
2019-10-18 17:10:06 +0900

    doing 1.1

---
+++ Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading(){
+    return <View style={styles.container}>
+        <Text>Getting the current Weather</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1
+    }
+});
