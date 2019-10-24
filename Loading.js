a31f2d54c7e09c9061cc9538dbda3cd51ac550f0
정연수 <jys1015@hanyang.co.kr>
2019-10-24 16:44:07 +0900

    styled component

--- Loading.js
+++ Loading.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import styled from "styled-components"
+
+const Container = styled.View`
+    flex:1;
+    justifyContent:"flex-end";
+    paddingHorizontal: 30;
+    paddingVertical: 100;
+    backgroundColor:"#FDF6AA";
+`;
+
+const LoadingText = styled.Text`
+    color:"#2c2c2c";
+    fontSize: 30;
+`
 
 export default function Loading(){
     return (
-    <View style={styles.container}>
+    //<View style={styles.container}>
+    //<Text style={styles.text}>Getting the current Weather</Text>
+    //</View>
+    <Container>
         <StatusBar barStyle="dark-content"/>
-        <Text style={styles.text}>Getting the current Weather</Text>
-    </View>
+        <LoadingText>Getting the current Weather</LoadingText>        
+    </Container>
+    
     );
 }
 
@@ -19,7 +37,6 @@ const styles = StyleSheet.create({
         backgroundColor:"#FDF6AA"
     },
     text:{
-        color:"#2c2c2c",
-        fontSize: 30
+        
     }
 });
